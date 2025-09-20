bd38b970e8c4350f8d61e7779ce197044e5664f4
takeda6969 <tim.kov07@gmail.com>
2025-09-20 13:08:03 +0300

    add leson repeat

--- src/App.kt
+++ src/App.kt
@@ -1,5 +1,76 @@
 fun main() {
-    val nambr = readln().toInt()
-    println(nambr)
-    println("hgnj")
+//    print("Введите 1 число : ")
+//    var n= readln().toInt()
+//    print("Введите 2 число : ")
+//    var n1= readln().toInt()
+//    if (n>n1){
+//        println("1 число больше")
+//    }
+//    else if (n==n1){
+//        println("Числа равны1")
+//    }
+//    else{
+//        println("2 число больше")
+//    }
+//
+//    val max= if (n>n1) n else n1
+//    println("Максимальное число: $max")
+//    val age= readln().toInt()
+//    val status= if (age>=18)"Взрослый" else "ребенок"
+//    println(status)
+
+//val nambr = 1 until 100
+//var num1= 100 downTo 1 step 5
+//val str= "a".."z"
+//for (i in nambr){
+//    println(i)
+//}
+//
+//    print("Ввкдите порядковый номер месяца (1 до 12): ")
+//    val indx = readln().toInt()
+//    var month =when(indx){
+//        1 -> "январь"
+//        2 -> "февраль"
+//        3 ->  "март"
+//        4 ->"апрель"
+//        5 ->  "май"
+//        6 ->"июнь"
+//        7 ->  "июль"
+//        8 -> "август"
+//        9 ->"сентябрь"
+//        10 -> "октябрь"
+//        11 -> "ноябрь"
+//        12 -> "декабрь"
+//        else -> "отсутвует месяц"
+//  }
+//    if (indx in 1..12){
+//        println("Ваш индекс $indx соответствует месяцу $month")
+//    }
+
+
+//  №4
+//    print("Введите урон: ")
+//    val  n1 = readln().toInt()
+//    print("Щит активен (да или нет?): ")
+//    val  n2 = readln().lowercase()
+//    if  (n2=="да"){
+//        println("Полученый урон: ${n1/2}")
+//    }
+//    else{
+//        println("Полученый урон: $n1")
+//    }
+
+//
+//    print("Введите ваш уровень: ")
+//    val  level = readln().toInt()
+//    print("У вас есть VIP-статус (true/false): ")
+//    val  hasVip = readln().toBoolean()
+//    if ((level >= 30 && hasVIP)|| level >= 50){
+//        println("Доступ разрешен!")
+//    }
+//    else{
+//        println("Доступ запрещен")
+//    }
+
+
 }
---
+++ src/Range.kt
@@ -0,0 +1,14 @@
+fun main() {
+//  №5
+//    val n =1..100
+//    val s="a".."z"
+//    print("Введите ваш уровень: ")
+//    val  level = readln().toInt()
+//    if (level in 1..50){
+//        println("Уровень $level соответствует требованиям!")
+//    }
+//    else{
+//        println("Высокий уровень!")
+//    }
+
+}
